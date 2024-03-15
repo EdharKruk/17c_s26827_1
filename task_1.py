591d21045a4b2a25a2ce5372ac238ff2d3c6d5dd
Edhar Kruk <edgarkrook@gmail.com>
2024-03-15 13:07:37 +0100

    Complete task 2

--- task_1.py
+++ task_1.py
@@ -1,5 +1,17 @@
 
 
+def generate_squares(start, end):
+    return [i**2 for i in range(start, end + 1)]
+
+
+
 if __name__ == '__main__':
     squares = [i ** 2 for i in range(1, 11)]
     print(squares)
+    squares = generate_squares(1, 10)
+    print(squares)
+
+
+
+
+
