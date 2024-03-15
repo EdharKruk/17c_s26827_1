db0deff08635f89b8b11ece9fa55a8d61d783a1a
Edhar Kruk <edgarkrook@gmail.com>
2024-03-15 13:21:18 +0100

    Complete task 5

--- task_1.py
+++ task_1.py
@@ -4,11 +4,15 @@ import math
 class SquareGenerator:
     def generate_squares(self, start, end):
         if end < start:
-            return "End must be greater than start"
-        return [i**2 for i in range(start, end + 1)]
+            raise ValueError("End of range must be greater than start")
+        return [i ** 2 for i in range(start, end + 1)]
 
     def generate_square_roots(self, start, end):
-        return [math.sqrt(i) for i in self.generate_squares(start, end)]
+        try:
+            return [math.sqrt(i) for i in self.generate_squares(start, end)]
+        except ValueError as e:
+            print(e)
+            return []
 
 
 def generate_squares(start, end):
@@ -27,6 +31,11 @@ if __name__ == '__main__':
     generator = SquareGenerator()
     square_roots = generator.generate_square_roots(1, 10)
     print(square_roots)
+    generator = SquareGenerator()
+    try:
+        square_roots = generator.generate_square_roots(10, 1)
+    except ValueError as e:
+        print(e)
 
 
 
