1fa16b7acf9dbe3e446ab422cc512c806e5ae074
Edhar Kruk <edgarkrook@gmail.com>
2024-03-15 13:12:05 +0100

    Complete task 3

--- task_1.py
+++ task_1.py
@@ -1,4 +1,11 @@
 
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            return "End must be greater than start"
+        return [i**2 for i in range(start, end + 1)]
+
+
 
 def generate_squares(start, end):
     return [i**2 for i in range(start, end + 1)]
@@ -8,8 +15,15 @@ def generate_squares(start, end):
 if __name__ == '__main__':
     squares = [i ** 2 for i in range(1, 11)]
     print(squares)
-    squares = generate_squares(1, 10)
-    print(squares)
+    squares1 = generate_squares(1, 10)
+    print(squares1)
+    generator = SquareGenerator()
+    squares2 = generator.generate_squares(1, 10)
+    print(squares2)
+
+
+
+
 
 
 
